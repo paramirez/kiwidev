58732fa28623421f94d3cd5a297e9e4fa7ff74fd
Pablo Ramírez <pabloandres2700@gmail.com>
2020-04-02 03:11:54 -0500

    cambios nuevos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
+import logo from "./logo.svg";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>En construcción</code>
-        </p>
-        <a
-          className="App-link"
-          href="mailto:pablo.ramirez@kiwidev.co?Subject=Hola!"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Contacto
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          {/* <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav> */}
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <code>En construcción</code>
+          </p>
+          <Link className="App-link" style={{ display: "none" }} to="/about">
+            About
+          </Link>
+          <a
+            className="App-link"
+            href="mailto:pablo.ramirez@kiwidev.co?Subject=Hola!"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <code>Comunicante con nosotros</code>
+          </a>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+
+          {/* <Switch>
+            <Route path="/users">
+              <Users />
+            </Route>
+          </Switch> */}
+        </header>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function About() {
+  return (
+    <h2>
+      <code>Información adiconal</code>
+    </h2>
+  );
+}
+
+// function Users() {
+//   return <h2>Users</h2>;
+// }
